9986a6ee32a10a68fe53b63ed54d2505d7765e04
paydaylight <maxat235@gmail.com>
2021-02-11 18:30:10 +0600

    Detect bad alignment and autocorrect

--- lib/datarockets_style/cop/layout/block_alignment_in_hash.rb
+++ lib/datarockets_style/cop/layout/block_alignment_in_hash.rb
@@ -3,7 +3,9 @@ module DatarocketsStyle
     module Layout
       class BlockAlignmentInHash < RuboCop::Cop::Cop
         include RuboCop::Cop::Alignment
-        MSG = "Align"
+        include RuboCop::Cop::EndKeywordAlignment
+
+        MSG = "Align".freeze
         def_node_matcher :hash_with_block?, <<-PATTERN
           (hash
             <(pair
@@ -15,13 +17,21 @@ module DatarocketsStyle
 
         def on_hash(node)
           return unless hash_with_block?(node)
+
+          node.children.each do |pair_node|
+            _key_node, value_node = pair_node.children
+
+            next unless value_node.is_a? RuboCop::AST::BlockNode
+            next unless begins_its_line?(value_node.loc.end)
+
+            check_alignment(value_node.children, value_node.loc.column)
+          end
         end
 
-        # def autocorrect(node)
-        #   RuboCop::Cop::AlignmentCorrector.correct(processed_source, node, column_delta)
-        # end
+        def autocorrect(node)
+          RuboCop::Cop::AlignmentCorrector.correct(processed_source, node, column_delta + configured_indentation_width)
+        end
       end
     end
   end
 end
-
